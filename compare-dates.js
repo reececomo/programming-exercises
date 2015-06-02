a591efb6863c606eaa37c2c78ccd024d45bf14c3
Reece Como <reece.notargiacomo@gmail.com>
2015-06-02 14:10:42 +0800

    Update compare-dates.js

--- compare-dates.js
+++ compare-dates.js
@@ -1,40 +1,39 @@
 /*
 *  Demonstration
 */
-var day1 = { d:1, m:1, yr:2012 };
-var day2 = { d:1, m:1, yr:2013 };
+var date1 = "1/1/2012";
+var date2 = "1/1/2013";
 
-var day1String = day1.d+"/"+day1.m+"/"+day1.yr;
-var day2String = day2.d+"/"+day2.m+"/"+day2.yr;
-
-console.log("Calculate the number of days between "+day1String+" and "+day2String);
-var result = getDifferenceAsDays(day1,day2);
+console.log("Calculate the number of days between "+date1+" and "+date2);
+var result = getDifferenceAsDays(date1,date2);
 console.log("Result: " + result);
 
 /*
 *  Algorithm for counting the number
 *      of days between two days
 */
-function getDifferenceAsDays(A, B) {
+function getDifferenceAsDays(DateA,DateB) {
+    // Convert strings into date object
+    _A = DateA.split("/"); A = {d:_A[0], m:_A[1], yr:_A[2]};
+    _B = DateB.split("/"); B = {d:_B[0], m:_B[1], yr:_B[2]};
     
     if(isValidDate(A) && isValidDate(B)) {
-        var minYear = Math.min(A.yr,B.yr);
-        return Math.abs(convertToDays(B, minYear) - convertToDays(A, minYear));
+        var min = Math.min(A.yr,B.yr);
+        return Math.abs(countDaysFrom(B, min) - countDaysFrom(A, min));
     }
       
     else throw "One (or more) of your dates were invalid!";
 }
 
-function convertToDays(date, minYear) {
+function countDaysFrom(date, year) {
     // A list of the number of days in months (Indexed from 1)
     var daysInMonth = [null,31,28,31,30,31,30,31,31,30,31,30,31];
-    
     var total = date.d; // add days
     
     for(var mo = 1; mo < date.m; mo++) // add months (in # of days)
         total += daysInMonth[mo];
     
-    for(var yr = minYear; yr < date.yr; yr++) // add years (in # of days)
+    for(var yr = year; yr < date.yr; yr++) // add years (in # of days)
         total += 365 + leapYear(yr);
     
     if(date.m>2) // EDGE CASE: February 29th in leap year
