40f16b2a096facbba9a682e36d25633f885655a5
Reece Como <reece.notargiacomo@gmail.com>
2015-06-03 15:39:30 +0800

    Create longest-common-subsequence.js

---
+++ longest-common-subsequence.js
@@ -0,0 +1,16 @@
+/*
+ *  Dynamic Programming:
+ *    Longest Common Subsequence problem
+ */
+function lcs(A,B) {
+    var n = A.length;
+    var m = B.length;
+    
+    var table = new Array();
+    
+    for(var i = 0; i < n; i++) {
+        var row = new Array(); // prepare a table row
+    }
+    
+    return result;
+}
